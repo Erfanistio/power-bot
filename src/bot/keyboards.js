caf361d7400d6082897b00f947fa77434b70f1be
Erfan Akrami <141495115+Erfanistio@users.noreply.github.com>
2026-08-18 01:11:47 +0330

    Change button text for adding first bookmark

--- src/bot/keyboards.js
+++ src/bot/keyboards.js
@@ -47,7 +47,7 @@ export function getStartInlineKeyboard(savedBills = []) {
       inline_keyboard: [
         [
           {
-            text: '🔵 📌 افزودن اولین بوکمارک ➕',
+            text: '🎟️ 📌 افزودن اولین بوکمارک ➕',
             callback_data: 'add_bill_prompt',
             style: 'primary'
           }
